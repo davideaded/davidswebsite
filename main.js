55dd9b34d494258dd59b641382d39cfa2f67b10b
davideaded <david.castrodc033@gmail.com>
2024-03-08 17:23:59 -0300

    Fixed dark mode, better navbar visualisation

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 const pages = ["Home", "About", "Projects"];
 const mobileNav = document.getElementById("mobile-nav");
+const mainNav = document.getElementById("main-nav");
 const mobileNavMenu = document.getElementById("menu");
 
-document.getElementById("theme-toggle").addEventListener("click", () => {
+const toggleDarkMode = () => {
   document.body.classList.toggle("dark-mode");
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const isDarkMode = localStorage.getItem("darkMode");
+  isDarkMode === "true" ? toggleDarkMode() : null;
 })
+
+document.getElementById("theme-toggle").addEventListener("click", () => {
+  toggleDarkMode();
+
+  const isDarkMode = document.body.classList.contains("dark-mode");
+  localStorage.setItem("darkMode", isDarkMode.toString());
+})
+
 document.getElementById("h-menu").addEventListener("click", () => {
   mobileNav.style.width = "100%";
   mobileNav.style.left = "0";
@@ -12,7 +26,7 @@ document.getElementById("h-menu").addEventListener("click", () => {
   const closeNav = document.getElementById("close-nav");
   closeNav.addEventListener("click", () => {
     mobileNav.style.width = "0";
-  mobileNav.style.left = "100%";
+    mobileNav.style.left = "100%";
   })
 })
 
@@ -23,7 +37,9 @@ const createNavBar = (parentDiv) => {
     const liElement = document.createElement("li");
     const aElement = document.createElement("a");
 
-    aElement.href = `${page.toLowerCase()}`;
+    liElement.classList = page.toLowerCase();
+
+    aElement.href = page.toLowerCase();
     aElement.textContent = page;
 
     liElement.appendChild(aElement);
@@ -33,4 +49,18 @@ const createNavBar = (parentDiv) => {
   parentDiv.appendChild(ulElement);
 }
 
+const displayCurrentPage = () => {
+  const currentPath = window.location.pathname.substring(1).toLowerCase();
+  console.log(currentPath);
+
+  const linkElements = document.querySelectorAll(`.${currentPath}`);
+  if (linkElements) {
+    linkElements.forEach(e => e.classList.add('current-page'));
+  } else {
+    console.log("Current element not found");
+  }
+}
+
 createNavBar(mobileNavMenu);
+createNavBar(mainNav);
+displayCurrentPage();
