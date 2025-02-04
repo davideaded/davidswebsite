1aea5122c047fcbe1d03ebc6dcaf19bb9da49ec7
David <108702656+davideaded@users.noreply.github.com>
2025-02-04 11:39:48 -0300

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -3,14 +3,7 @@ export default function loadAbout() {
   const paragraph = document.createElement("p");
   const image = document.createElement("img");
   paragraph.innerHTML = `
-    I was adrift, lacking direction and purpose, until a developer friend introduced me to the programming world.
-    Descending that spiral, I found myself
-    unraveling code, being amused by reading and understanding existing code, as well bringing my own chaotic creations to life.
-    <br><br>
-    Seeking to deepen my understanding, I enrolled in an online bootcamp at Soul Code Academy, where I immersed myself in the fundamentals of web development over the course
-    of several months. Yet, despite this initial progress, I soon was imprisioned in the "tutorial hell."
-    Fortunately I've discovered The Odin Project. In between an abundance of online courses, I found a path of learning through creation,
-    allowing me to truly grasp the concepts by applying them in real projects.
+    lol
   `
     ;
   div.id = "about-page";
