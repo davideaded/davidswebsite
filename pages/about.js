91bab83b4704a8378afaac89d31fde2ecf8def1e
davideaded <david.castrodc033@gmail.com>
2025-03-26 12:28:11 -0300

    general improvements

--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,7 @@ export default function loadAbout() {
   const paragraph = document.createElement("p");
   const image = document.createElement("img");
   paragraph.innerHTML = `
-    lol
+    With a late-blooming interest in technology — mainly programming and the internet — I spent some time studying on my own before enrolling in a Computer Science bachelor's program. Now, I’ve decided to pursue it as a career rather than just a hobby, aiming to contribute meaningfully by creatively applying my skills to bring projects and applications to life.
   `
     ;
   div.id = "about-page";
--- pages/home.js
+++ pages/home.js
@@ -3,10 +3,7 @@ export default function loadHome() {
 
   paragraph.innerHTML = `
     Hey there! I'm a developer based in São Paulo, Brazil. 
-    I'm diving into the world of technology, keen to explore its vast oceans and apply its power to create impactful solutions. And have some fun, of course. <br><br>
-
-    From crafting intuitive interfaces to building robust back-end systems - or attempting to, at least -, I walk through the digital landscape with curiosity and determination. <br><br>
-    
+    I am currently a Computer Science student at UNICID.
     Here, I'll be showcasing some of my projects. I'm certain it will be amusing to look back at my old code in the years to come. <br><br>
 
     Have fun looking around!
